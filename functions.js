0eb29905a5316c7b25c1cb6d8d01ab343d85858f
ComicallyBad <amp5646@yahoo.com>
2020-03-02 18:49:45 -0800

    Weird update bug in discord.js

--- functions.js
+++ functions.js
@@ -130,7 +130,7 @@ module.exports = {
         const filter = (reaction, user) => validReactions.includes(reaction.emoji.name) && user.id === author.id;
 
         return message
-            .awaitReactions(filter, { max: 1, time: time })
+            .awaitReactions(filter, { max: 2, time: time })
             .then(collected => collected.first() && collected.first().emoji.name);
     },
 
