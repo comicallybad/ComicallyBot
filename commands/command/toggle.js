851ecbc16156e41c26817d99abe29e3a36128dbd
amp5646@yahoo.com <amp5646@yahoo.com>
2020-03-10 22:07:14 -0500

    Added ability to toggle commands by alias

--- commands/command/toggle.js
+++ commands/command/toggle.js
@@ -14,11 +14,13 @@ module.exports = {
         const logChannel = message.guild.channels.cache.find(c => c.name === "mods-log") || message.channel;
         let guildID = message.guild.id;
         let commands = client.commands.map(cmd => cmd.name);
+        let aliases = client.commands.map(cmd => cmd.aliases);
+        let aliasList = [].concat.apply([], aliases)
 
         if (!args[0])
             return message.reply("Please provide the command you wish to toggle.").then(m => m.delete({ timeout: 7500 }));
 
-        if (!commands.includes(args[0]))
+        if (!commands.includes(args[0]) && !aliasList.includes(args[0]))
             return message.reply("Please provide a valid command.").then(m => m.delete({ timeout: 7500 }));
 
         if (args[0] === "toggle")
@@ -61,6 +63,34 @@ module.exports = {
             logChannel.send(embed);
 
             return message.reply("Toggling command... this may take a second...").then(m => m.delete({ timeout: 7500 }));
+        } else if (aliasList.includes(args[0])) {
+            let command = client.commands.map(function (cmd) {
+                if (cmd.aliases == args[0] && cmd.category !== "command") return cmd.name
+            }).filter(cmd => cmd)[0];
+            let bool;
+
+            if (!command)
+                return message.reply("You cannot disable a command under the command category").then(m => m.delete({ timeout: 7500 }));
+            else {
+                if (args[1] === "true" || args[1] === "enable") bool = true;
+                if (args[1] === "false" || args[1] === "disable") bool = false;
+
+                db.updateOne({ guildID: guildID, 'commands.name': command }, {
+                    $set: { 'commands.$.status': bool }
+                }).catch(err => console.log(err));
+
+                const embed = new MessageEmbed()
+                    .setColor("#0efefe")
+                    .setThumbnail(message.author.displayAvatarURL())
+                    .setFooter(message.member.displayName, message.author.displayAvatarURL())
+                    .setTimestamp()
+                    .setDescription(stripIndents`**> Command Toggled by:** ${message.member.user.username} (${message.member.id})
+                    **> Command Toggled:** ${command}`);
+
+                logChannel.send(embed);
+
+                return message.reply("Toggling command... this may take a second...").then(m => m.delete({ timeout: 7500 }));
+            }
         }
     }
 }
