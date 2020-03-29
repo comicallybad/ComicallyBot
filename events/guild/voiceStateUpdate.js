e321c01f053289dd1a62c23200e13ce06bd7fcc9
amp5646@yahoo.com <amp5646@yahoo.com>
2020-03-29 13:20:41 -0500

    music now will reset timeout if a user rejoins

--- events/guild/voiceStateUpdate.js
+++ events/guild/voiceStateUpdate.js
@@ -1,38 +1,62 @@
 module.exports = (client, voiceStateStart, voiceStateEnd) => {
-    if (!voiceStateStart.channelID && voiceStateEnd.channelID) { //If user joins a channel
+    let startChannel = voiceStateStart.channelID;
+    let endChannel = voiceStateEnd.channelID;
+
+    if (!startChannel && endChannel) { //If user joins a channel
         if (voiceStateEnd.id == client.user.id) { //If the user is the bot
-            if (!voiceChannels.some(channel => channel.channelID === voiceStateStart.channelID))
-                voiceChannels.push({ channelID: voiceStateEnd.channelID, users: client.channels.cache.get(voiceStateEnd.channelID).members.size })
+            if (!channelsSome(startChannel)) {
+                voiceChannels.push({ channelID: endChannel, users: getSize(client, endChannel) })
+            }
         } else { //Update user count
-            if (voiceChannels.find(channel => channel.channelID == voiceStateEnd.channelID))
-                voiceChannels.find(channel => channel.channelID == voiceStateEnd.channelID).users += 1;
+            if (channelsFind(endChannel)) {
+                channelsFind(endChannel).users += 1;
+            }
         }
-    } else if (voiceStateStart.channelID && !voiceStateEnd.channelID) { //If user disconnects
+    } else if (startChannel && !endChannel) { //If user disconnects
         if (voiceStateStart.id == client.user.id) { //If the user is the bot
             const guildID = voiceStateEnd.guild.id;
             const player = client.music.players.get(guildID);
-            if (player) client.music.players.destroy(guildID);
-            if (voiceChannels.some(channel => channel.channelID === voiceStateStart.channelID))
-                voiceChannels.splice(voiceChannels.findIndex(channel => channel.channelID === voiceStateStart.channelID), 1)
+            if (player) client.music.players.destroy(guildID); {
+                if (channelsSome(startChannel)) {
+                    channelsSplice(startChannel)
+                }
+            }
         } else {//Update user count
-            if (voiceChannels.find(channel => channel.channelID == voiceStateStart.channelID)) {
-                voiceChannels.find(channel => channel.channelID == voiceStateStart.channelID).users -= 1;
+            if (channelsFind(startChannel)) {
+                channelsFind(startChannel).users -= 1;
             }
         }
-    } else if (voiceStateStart.channelID && voiceStateEnd.channelID) {//If user moves
-        if (voiceStateStart.id == client.user.id && voiceStateStart.channelID) { //If the user is the bot
-            if (voiceChannels.some(channel => channel.channelID === voiceStateStart.channelID))
-                voiceChannels.splice(voiceChannels.findIndex(channel => channel.channelID === voiceStateStart.channelID), 1)
-            voiceChannels.push({ channelID: voiceStateEnd.channelID, users: client.channels.cache.get(voiceStateEnd.channelID).members.size })
+    } else if (startChannel && endChannel) {//If user moves
+        if (voiceStateStart.id == client.user.id && startChannel) { //If the user is the bot
+            if (channelsSome(startChannel)) {
+                channelsSplice(startChannel);
+            }
+            voiceChannels.push({ channelID: endChannel, users: getSize(client, endChannel) })
         } else {//Update user count on both start and end channel
-            if (voiceChannels.find(channel => channel.channelID == voiceStateStart.channelID)) {
-                voiceChannels.find(channel => channel.channelID == voiceStateStart.channelID).users = client.channels.cache.get(voiceStateStart.channelID).members.size;
-            } else if (voiceChannels.find(channel => channel.channelID == voiceStateEnd.channelID)) {
-                voiceChannels.find(channel => channel.channelID == voiceStateEnd.channelID).users = client.channels.cache.get(voiceStateEnd.channelID).members.size;
+            if (channelsSome(startChannel)) {
+                channelsFind(startChannel).users = getSize(client, startChannel);
+            } else if (channelsSome(endChannel)) {
+                channelsFind(endChannel).users = getSize(client, endChannel);
             }
         }
     }
     checkUsers(client);
+}
+
+function getSize(client, channel) {
+    return client.channels.cache.get(channel).members.size;
+}
+
+function channelsFind(channelID) {
+    return voiceChannels.find(channel => channel.channelID == channelID)
+}
+
+function channelsSome(channelID) {
+    return voiceChannels.some(channel => channel.channelID === channelID)
+}
+
+function channelsSplice(channelID) {
+    voiceChannels.splice(voiceChannels.findIndex(channel => channel.channelID === channelID), 1)
 }
 
 function checkUsers(client) {
@@ -44,8 +68,16 @@ function checkUsers(client) {
 
 function checkDisconnect(client, channelID) {
     let disconnectChannel = setTimeout(function () {
-        disconnect(client, channelID)
-    }, 900000); //15 minutes 
+        clearInterval(intervalCheck);
+        disconnect(client, channelID);
+    }, 900000); //15 minutes == 900000
+    let intervalCheck = setInterval(function () {
+        var size = getSize(client, channelID)
+        if (size > 1) {
+            clearInterval(intervalCheck);
+            clearTimeout(disconnectChannel)
+        }
+    }, 1000);
 }
 
 function disconnect(client, channelID) {
@@ -55,7 +87,6 @@ function disconnect(client, channelID) {
             const player = client.music.players.get(guildID);
             if (player) client.music.players.destroy(guildID);
             else client.channels.cache.get(channelID).disconnect()
-            if (voiceChannels.some(channel => channel.channelID === channelID))
-                voiceChannels.splice(voiceChannels.find(channel => channel.channelID === channelID), 1)
+            if (channelsSome(channelID)) channelsSplice(channelID)
         }
 }
