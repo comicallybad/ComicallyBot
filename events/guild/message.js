0e45b06aad118550cacbf001fc22fc940687563a
amp5646@yahoo.com <amp5646@yahoo.com>
2020-03-28 19:45:17 -0500

    Minor updates

--- events/guild/message.js
+++ events/guild/message.js
@@ -7,7 +7,7 @@ module.exports = async (client, message) => {
 
     addCoins(message, client)
 
-    if (!message.content.startsWith(prefix) && !message.content.startsWith("<@!492495421822730250>")) return;
+    if (!message.content.startsWith(prefix) && !message.content.startsWith(`<@!${client.user.id}>`)) return;
     if (!message.member) message.member = await message.guild.fetchMember(message);
 
     const channelPermissions = message.channel.permissionsFor(message.guild.me);
@@ -15,7 +15,7 @@ module.exports = async (client, message) => {
     if (!channelPermissions.has("SEND_MESSAGES") || !channelPermissions.has("MANAGE_MESSAGES") || !channelPermissions.has("ADD_REACTIONS"))
         return message.author.send("I am missing permissions to either `SEND_MESSAGES`, `MANAGE_MESSAGES` or `ADD_REACTIONS`.").then(m => del(m, 60000));
 
-    const args = message.content.startsWith(prefix) ? message.content.slice(prefix.length).trim().split(/ +/g) : message.content.slice("<@!492495421822730250>".length).trim().split(/ +/g);
+    const args = message.content.startsWith(prefix) ? message.content.slice(prefix.length).trim().split(/ +/g) : message.content.slice(`<@!${client.user.id}>`.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
 
     if (cmd.length === 0) return;
--- events/guild/voiceStateUpdate.js
+++ events/guild/voiceStateUpdate.js
@@ -1,12 +1,15 @@
 module.exports = (client, voiceStateStart, voiceStateEnd) => {
-    if (voiceStateEnd.id == "492495421822730250") {
-        const channelID = voiceStateEnd.guild.voiceStates.cache.map(user => user).filter(user => user.id == "492495421822730250")[0].channelID
-        if (channelID == null) {
-            const guildID = voiceStateEnd.guild.id;
-            const player = client.music.players.get(guildID);
+    if (voiceStateEnd.channelID == undefined) { //When a user disconnects
+        if (voiceStateEnd.id == client.user.id) { //If the user is the bot
+            const channelID = voiceStateStart.channelID
+            if (channelID == null) {//If the bot was disconnected
+                const guildID = voiceStateEnd.guild.id;
+                const player = client.music.players.get(guildID);
 
-            if (!player) return
-            else client.music.players.destroy(guildID);
+                //destroy the player (as it will keep trying to play)
+                if (!player) return
+                else client.music.players.destroy(guildID);
+            }
         }
     }
 }
