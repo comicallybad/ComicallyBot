47e66aaf96ead62137fc4ac9350b448d08ea5104
amp5646@yahoo.com <amp5646@yahoo.com>
2020-04-12 13:54:01 -0500

    Error handling

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,8 +1,18 @@
 const db = require("../../schemas/db.js");
 
 module.exports = async (client, message) => {
-    let guildID = message.guild.id;
-    let messageID = message.id;
+    let messageID;
+    let guildID;
+
+    if (message) { //fix for cannot read property ID of null
+        if (message.id) {
+            messageID = message.id;
+        } else return;
+        if (message.guild.id) {
+            guildID = message.guild.id
+        } else return;
+    } else return;
+
     db.updateOne({ guildID: guildID }, {
         $pull: { reactionRoles: { messageID: messageID } }
     }).catch(err => console.log(err))
