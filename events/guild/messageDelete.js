2ac0227a31f2eb23f042a1a5cff81564f57291d0
amp5646@yahoo.com <amp5646@yahoo.com>
2020-04-12 08:35:33 -0500

    Fixed messageDelete event

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,17 +1,6 @@
 const db = require("../../schemas/db.js");
 
 module.exports = async (client, message) => {
-    if (message.partial) {
-        message.fetch()
-            .then(fullMessage => {
-                checkReactionRole(fullMessage);
-            });
-    } else {
-        checkReactionRole(message);
-    }
-}
-
-function checkReactionRole(message) {
     let guildID = message.guild.id;
     let messageID = message.id;
     db.updateOne({ guildID: guildID }, {
