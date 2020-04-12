66660d5d33d9c00d71ace06aa5d0ef57cd8c7c58
amp5646@yahoo.com <amp5646@yahoo.com>
2020-04-12 08:56:46 -0500

    Error handling

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -47,11 +47,12 @@ module.exports = {
                 del(msg, 0);
 
                 //attempt unban and send message
-                bannedMember.send(`Hello, you have been **unbanned** in ${message.guild.name} for: **${reason}**`).catch(err => console.log(err));
                 message.guild.members.unban(bannedMember, reason)
                     .catch(err => {
                         if (err) return message.reply(`Well.... the unban didn't work out. Here's the error ${err}`).then(m => del(m, 7500));
                     });
+
+                bannedMember.send(`Hello, you have been **unbanned** in ${message.guild.name} for: **${reason}**`).catch(err => err); //in case DM's are closed
 
                 logChannel.send(embed);
             } else if (emoji === "❌") {
