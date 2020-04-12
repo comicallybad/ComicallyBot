8689537a92e2d8e47af59010540aef071659d702
amp5646@yahoo.com <amp5646@yahoo.com>
2020-04-12 08:15:44 -0500

    Message delete checks if message.partial now

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,6 +1,17 @@
 const db = require("../../schemas/db.js");
 
 module.exports = async (client, message) => {
+    if (message.partial) {
+        message.fetch()
+            .then(fullMessage => {
+                checkReactionRole(fullMessage);
+            });
+    } else {
+        checkReactionRole(message);
+    }
+}
+
+function checkReactionRole(message) {
     let guildID = message.guild.id;
     let messageID = message.id;
     db.updateOne({ guildID: guildID }, {
