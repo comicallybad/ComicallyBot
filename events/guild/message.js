89e3ad9e9b4bdf0e57ad90fc9ae21f0a427752a1
amp5646@yahoo.com <amp5646@yahoo.com>
2020-04-15 06:49:56 -0500

    Updated to allow @mention commands on phone

--- events/guild/message.js
+++ events/guild/message.js
@@ -7,10 +7,10 @@ module.exports = async (client, message) => {
 
     addCoins(message, client)
 
-    if (!message.content.startsWith(prefix) && !message.content.startsWith(`<@!${client.user.id}>`)) return;
+    if (!message.content.startsWith(prefix) && !message.content.replace(/\D/g, '').startsWith(`${client.user.id}`)) return;
     if (!message.member) message.member = await message.guild.fetchMember(message).catch(err => err);
 
-    const args = message.content.startsWith(prefix) ? message.content.slice(prefix.length).trim().split(/ +/g) : message.content.slice(`<@!${client.user.id}>`.length).trim().split(/ +/g);
+    const args = message.content.startsWith(prefix) ? message.content.slice(prefix.length).trim().split(/ +/g) : message.content.replace(/[^\s]*/, '').trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
 
     if (cmd.length === 0) return;
