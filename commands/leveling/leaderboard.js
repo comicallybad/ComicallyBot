e0e74bc43ca6f8c3b064ffa8b15cacd5c9918c01
amp5646@yahoo.com <amp5646@yahoo.com>
2020-05-09 15:37:44 -0500

    leaderboard command test

---
+++ commands/leveling/leaderboard.js
@@ -0,0 +1,40 @@
+const { stripIndents } = require("common-tags");
+const { MessageEmbed } = require("discord.js");
+const xp = require('../../schemas/xp.js');
+
+module.exports = {
+    name: "leaderboard",
+    aliases: ["leaderboards", "xpleaderboard", "xpleaderboards"],
+    category: "leveling",
+    description: "Shows the top level users in the discord server.",
+    permissions: "moderator",
+    usage: "<@user|userID> <amount>",
+    run: (client, message, args) => {
+        let guildID = message.guild.id;
+        xp.find({ guildID: guildID }, (err, exists) => {
+            if (exists) {
+                const embed = new MessageEmbed()
+                    .setTitle("XP Leaderboard")
+                    .setColor("#0efefe")
+                    .setTimestamp()
+
+                let sorted = exists.sort(user => user.xp)
+                let count = 1;
+                if (sorted.length >= 10) {
+                    for (i = sorted.length - 1; i > sorted.length - 11; i--) {
+                        embed.addField(`#${count}`, `**${sorted[i].userName}, level: ${sorted[i].level}, XP: ${sorted[i].xp}**`)
+                        count++;
+                    }
+                } else {
+                    for (i = sorted.length - 1; i >= 0; i--) {
+                        embed.addField(`**#${count}**`, `**${sorted[i].userName}, level: ${sorted[i].level}, XP: ${sorted[i].xp}**`)
+                        count++;
+                    }
+                }
+                message.channel.send(embed);
+            } else {
+                return message.reply("There was an error finding users within this server.").then(m => del(m, 7500));
+            }
+        })
+    }
+}
