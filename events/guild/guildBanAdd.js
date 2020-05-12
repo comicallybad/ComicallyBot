4de03e1f5a46beb6232b471aa01b06283c7d0e7f
amp5646@yahoo.com <amp5646@yahoo.com>
2020-05-12 16:56:48 -0500

    Logging for bans

---
+++ events/guild/guildBanAdd.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = async (client, guild, user) => {
+    if (guild.channels) {
+        let logChannel = await guild.channels.cache.find(c => c.name === "mod-logs" || undefined);
+
+        if (logChannel) {
+            const embed = new MessageEmbed()
+                .setColor("#0efefe")
+                .setTitle("User Banned")
+                .setThumbnail(user.displayAvatarURL())
+                .setDescription(`${user} ${user.tag}`)
+                .setFooter(`ID: ${user.id}`)
+                .setTimestamp()
+
+            logChannel.send(embed);
+        }
+    }
+}
