6396f57c3f589a3650036f8fcc6786a93ad32d3f
amp5646@yahoo.com <amp5646@yahoo.com>
2020-05-16 01:20:54 -0500

    Suggestions command!

---
+++ commands/helpful/suggest.js
@@ -0,0 +1,42 @@
+const { del } = require("../../functions.js");
+const { MessageEmbed } = require("discord.js");
+const db = require('../../schemas/db.js');
+
+module.exports = {
+    name: "suggest",
+    aliases: ["suggestion", "addsuggestion", "idea"],
+    category: "helpful",
+    description: "Sends a message with a suggestion users can vote on.",
+    permissions: "member",
+    usage: "<suggestion>",
+    run: (client, message, args) => {
+        let guildID = message.guild.id;
+
+        db.findOne({ guildID: guildID, channels: { $elemMatch: { command: "suggest" } } }, async (err, exists) => {
+            if (!exists) return message.reply("A suggestion channel has not been set by a moderator.").then(m => del(m, 7500));
+            if (exists) {
+                let channel = message.guild.channels.cache.get(exists.channels.filter(cmd => cmd.command == "suggest")[0].channelID);
+
+                if (message.channel.id !== channel.id)
+                    return message.reply(`This command is only available in the ${channel} channel.`).then(m => del(m, 7500));
+
+                if (!args[0])
+                    return message.reply("Please provide a suggestion.").then(m => del(m, 7500));
+
+                let suggestion = args.join(" ");
+
+                const embed = new MessageEmbed()
+                    .setColor("#0efefe")
+                    .setAuthor(`${message.member.user.tag}`, `${message.author.displayAvatarURL()}`)
+                    .setDescription(suggestion)
+                    .setFooter(`Select a reaction below to vote on ${message.author.username}'s suggestion`)
+                    .setTimestamp()
+
+                if (args[0]) {
+                    let m = await message.channel.send(embed);
+                    m.react("✅").then(m.react("❌"));
+                }
+            }
+        }).catch(err => err);
+    }
+}
