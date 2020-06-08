30d07bf99d53c7ddaa1f585974ba72036c19677d
amp5646@yahoo.com <amp5646@yahoo.com>
2020-06-08 07:57:00 -0500

    Bot messages can now be stopped from being deleted

--- functions.js
+++ functions.js
@@ -4,8 +4,13 @@ module.exports = {
     del: async function (message, timeout) {
         if (message) { //Fix in case bad message
             if (message.id) { //Fix cannot read ID 
-                if (message.deletable) message.delete({ timeout: timeout })
-                    .catch(err => err) //This gets rid of the annoying "Unknown Message" error.
+                if (message.deletable) {
+                    setTimeout(function () {
+                        if (!message.reactions.cache.get('🛑')) {  //messages can now stop from being deleted
+                            message.delete({ timeout: 0 }).catch(err => err) //This gets rid of the annoying "Unknown Message" error.
+                        }
+                    }, timeout);
+                }
             } else return;
         } else return;
     },
