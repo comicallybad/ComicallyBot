8668a9ccadc506e83d88fb1cbaa8e305a2e38f53
amp5646@yahoo.com <amp5646@yahoo.com>
2020-06-09 17:31:57 -0500

    Updated help additional info

--- commands/info/help.js
+++ commands/info/help.js
@@ -25,9 +25,11 @@ module.exports = {
 
 async function getAll(client, message) {
     const embed = new MessageEmbed()
-        .addField("Additional information", "Members must be added to gain access to \`member\` commands, and " +
-            `Moderators must be added to gain access to \`moderator\` commands via \`${prefix}addmember\` and \`${prefix}addmod\`. ` +
-            `Use \`${prefix}help <command name | alias>\` to view additional help and permissions for a certain command.`)
+        .addField("Additional information",
+            `Members must be added to gain access to \`member\` commands via \`${prefix}addmember\`.\n` +
+            `Moderators must be added to gain access to \`moderator\` commands via \`${prefix}addmod\`.\n ` +
+            `Use \`${prefix}help <command name|alias>\` to view help/permissions for commands.\n` +
+            `Add a \`🛑\` reaction to bot messages to prevent them from being deleted.`)
         .setColor("#0efefe")
         .setTitle("Help")
         .setTimestamp();
