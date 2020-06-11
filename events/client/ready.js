d8a1379625b7b763608ff8b49c0cb5eddbf007fe
ComicallyBad <amp5646@yahoo.com>
2020-06-11 12:40:50 -0500

    Added time bot went online

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,9 +2,18 @@ const { del } = require("../../functions.js");
 const mongoose = require("mongoose");
 const { dbSetup } = require("../../dbFunctions.js");
 const { ErelaClient, Utils } = require("erela.js");
+const { stripIndents } = require("common-tags")
 
 module.exports = client => {
-    console.log(`${client.user.username} online.`);
+    const dt = new Date()
+    console.log(stripIndents`${client.user.username} online. 
+    ${(dt.getMonth() + 1).toString().padStart(2, '0')}/${
+        dt.getDate().toString().padStart(2, '0')}/${
+        dt.getFullYear().toString().padStart(4, '0')} ${
+        dt.getHours().toString().padStart(2, '0')}:${
+        dt.getMinutes().toString().padStart(2, '0')}:${
+        dt.getSeconds().toString().padStart(2, '0')}`
+    );
 
     global.activities = [`${client.guilds.cache.size} servers!`, `${client.channels.cache.size} channels!`, `${client.users.cache.size} users!`], i = 0;
     setInterval(() => client.user.setActivity(`${prefix}help | ${activities[i++ % activities.length]}`, { type: "PLAYING" }), 7500)
