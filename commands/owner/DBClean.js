118d8845006eae32ec55b93ee5b9cf9075e30057
ComicallyBad <amp5646@yahoo.com>
2020-06-11 13:15:51 -0500

    Added DB Cleaunup commands

---
+++ commands/owner/DBClean.js
@@ -0,0 +1,27 @@
+const { del } = require("../../functions.js");
+const db = require("../../schemas/db.js")
+
+module.exports = {
+    name: "dbclean",
+    aliases: ["cleanupdb", "dbcleaup"],
+    category: "owner",
+    description: "Cleans DB database from unused guilds.",
+    permissions: "admin",
+    run: (client, message, args) => {
+        if (message.author.id != process.env.USERID)
+            return message.reply("You're not the bot the owner!").then(m => del(m, 7500));
+
+        const currentGuilds = client.guilds.cache.map(guild => guild.id)
+
+        db.find().then(data => {
+            const dbGuilds = data.map(guild => guild.guildID)
+            dbGuilds.forEach(ID => {
+                if (!currentGuilds.includes(ID)) {
+                    db.deleteOne({ guildID: ID })
+                        .catch(err => message.reply(`There was an error cleaning empty database listings. ${err}`).then(m => del(m, 7500)))
+                }
+            })
+            return message.reply("DB database has been cleaned up.").then(m => del(m, 7500))
+        }).catch(err => message.reply(`There was an error cleaning empty database listings. ${err}`).then(m => del(m, 7500)))
+    }
+}
---
+++ commands/owner/XPClean.js
@@ -0,0 +1,18 @@
+const { del } = require("../../functions.js");
+const xp = require("../../schemas/xp.js")
+
+module.exports = {
+    name: "xpclean",
+    aliases: ["cleanxp", "cleanxpuop", "xpcleanup"],
+    category: "owner",
+    description: "Cleans XP database from unused users.",
+    permissions: "admin",
+    run: (client, message, args) => {
+        if (message.author.id != process.env.USERID)
+            return message.reply("You're not the bot the owner!").then(m => del(m, 7500));
+
+        xp.deleteMany({ level: 0, xp: 0 })
+            .catch(err => message.reply(`There was an error deleting unused XP documents ${err}.`).then(m => del(m, 7500)))
+            .then(() => message.reply("XP database has been cleaned up.").then(m => del(m, 7500)))
+    }
+}
