d4c7b2e644c53c3494528a1665b17f86aa572475
amp5646@yahoo.com <amp5646@yahoo.com>
2020-06-13 17:37:02 -0500

    Nickname command

---
+++ commands/helpful/nickname.js
@@ -0,0 +1,46 @@
+const { del } = require("../../functions");
+const { MessageEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+    name: "nickname",
+    aliases: ["nick", "changename"],
+    category: "helpful",
+    description: "Changes a user's nickname.",
+    permissions: "moderator",
+    usage: "<@user | userID> <nickname>",
+    run: async (client, message, args) => {
+        if (!args[0])
+            return message.reply("Please provide a user.").then(m => del(m, 7500));
+
+        if (!args[1])
+            return message.reply("Please provide a new nickname.").then(m => del(m, 7500));
+
+        const logChannel = message.guild.channels.cache.find(c => c.name === "mod-logs") || message.channel;
+
+        if (!message.guild.me.hasPermission("MANAGE_NICKNAMES"))
+            return message.reply("I don't have permission to manage nicknames!").then(m => del(m, 7500));
+
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if (!user) return message.reply("Please supply a user to be banned!").then(m => del(m, 7500));
+
+        const embed = new MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle("User Nickname Changed")
+            .setThumbnail(user.user.displayAvatarURL())
+            .setFooter(message.member.displayName, message.author.displayAvatarURL())
+            .setTimestamp()
+            .setDescription(stripIndents`
+            **Member who's nicnkame was changed: ${user} (${user.id})**
+            **Nickname changed to: ${user.nickName}**
+            **Nickname changed by: ${message.author}**`);
+
+        let nickName = args.splice(1).join(' ');
+        user.setNickname(nickName, '')
+            .then(() => {
+                message.reply("User's nickname was successfully changed.").then(m => del(m, 7500));
+                logChannel.send(embed);
+            })
+            .catch(err => message.reply(`There was an error changing that users nickname: ${err}`).then(m => del(m, 7500)));
+    }
+}
