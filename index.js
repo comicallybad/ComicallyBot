2ef60eb6569535e51a4bf4fe24590a0053784ac1
amp5646@yahoo.com <amp5646@yahoo.com>
2020-06-13 16:17:31 -0500

    fixed prefix...

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { config } = require("dotenv");
 const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 config({ path: __dirname + "/.env" });
-global.prefix = "=";
+global.prefix = "_";
 global.voiceChannels = [];
 
 ["aliases", "commands"].forEach(x => client[x] = new Collection());
