e0dcf7b67e006179a0c5806f12644521126c51f9
amp5646@yahoo.com <amp5646@yahoo.com>
2020-06-24 18:22:20 -0500

    Updated Anti-Spam

--- dbFunctions.js
+++ dbFunctions.js
@@ -253,7 +253,7 @@ module.exports = {
                             spamUsers.splice(spamUsers.findIndex(user => user.id === message.author.id), 1)
                     }, 5000)
 
-                    if (cooldown.has(message.author.id) && spamUsers.find(user => user.id === message.author.id).offences >= 3) {
+                    if (cooldown.has(message.author.id) && spamUsers.find(user => user.id === message.author.id).offences >= 5) {
                         warn(message, spamOffencers, "spam")
                     }
                 }
