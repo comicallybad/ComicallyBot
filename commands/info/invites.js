4cd395b4869233760cdf7ebab543fa437feb79b8
comicallybad <amp5646@yahoo.com>
2020-08-07 10:29:05 -0400

    Update to invites.js

--- commands/info/invites.js
+++ commands/info/invites.js
@@ -7,7 +7,13 @@ module.exports = {
     description: "Provides server invite links.",
     permissions: "member",
     run: async (client, message, args) => {
-        const invites = await message.guild.fetchInvites().then(invites => invites.map(invite => `${invite} made by ${invite.inviter} with ${invite.uses} uses.`))
-        message.channel.send(invites).then(m => del(m, 30000))
+        const amount = await message.guild.fetchInvites().then(invites => invites.size);
+
+        if (amount > 0) {
+            const invites = await message.guild.fetchInvites().then(invites => invites.map(invite => `${invite} made by ${invite.inviter} with ${invite.uses} uses.`))
+            return message.channel.send(invites).then(m => del(m, 30000))
+        } else {
+            return message.reply("There are no server invites created.").then(m => del(m, 30000))
+        }
     }
 }
