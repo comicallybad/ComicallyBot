cfe5fba1a7a2e5a7bf8b5f7f3e5b1837c95a8b9b
amp5646@yahoo.com <amp5646@yahoo.com>
2020-11-15 13:05:37 -0500

    Updated clear to delete specific user messages

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -6,20 +6,38 @@ module.exports = {
     category: "moderation",
     description: "Clears the chat.",
     permissions: "moderator",
-    usage: "[number of messages]",
+    usage: "[@user | userID]<number of messages>",
     run: async (client, message, args) => {
-        if (isNaN(args[0]) || parseInt(args[0]) <= 0)
-            return message.reply("Please provide a valid number.").then(m => del(m, 7500));
+        if (!args[1]) {
+            if (isNaN(args[0]) || parseInt(args[0]) <= 0)
+                return message.reply("Please provide a valid number.").then(m => del(m, 7500));
 
-        if (!message.guild.me.hasPermission("MANAGE_MESSAGES"))
-            return message.reply("I do not have permissions to delete messages.").then(m => del(m, 7500));
+            if (!message.guild.me.hasPermission("MANAGE_MESSAGES"))
+                return message.reply("I do not have permissions to delete messages.").then(m => del(m, 7500));
 
-        let deleteAmount;
+            let deleteAmount;
 
-        if (parseInt(args[0]) > 100) deleteAmount = 100;
-        else deleteAmount = parseInt(args[0]);
+            if (parseInt(args[0]) > 100) deleteAmount = 100;
+            else deleteAmount = parseInt(args[0]);
 
-        message.channel.bulkDelete(deleteAmount, true)
-            .catch(err => message.reply(`Something went wrong ${err}`).then(m => del(m, 7500)));
+            message.channel.bulkDelete(deleteAmount, true)
+                .catch(err => message.reply(`Something went wrong ${err}`).then(m => del(m, 7500)));
+        } else if (args[1]) {
+            if (isNaN(args[1]) || parseInt(args[1]) <= 0)
+                return message.reply("Please provide a valid number.").then(m => del(m, 7500));
+
+            if (!message.guild.me.hasPermission("MANAGE_MESSAGES"))
+                return message.reply("I do not have permissions to delete messages.").then(m => del(m, 7500));
+
+            if (message.mentions.users.first()) {
+                message.channel.messages.fetch({ limit: 100 }).then((messages) => {
+                    const filterBy = message.mentions.users.first() ? message.mentions.users.first().id : Client.user.id;
+                    messages = messages.filter(m => m.author.id === filterBy).array().slice(0, args[1]);
+                    message.channel.bulkDelete(messages).catch(err => {
+                        return message.reply(`There was an error attempting to delete user messages: ${err}`)
+                    });
+                });
+            } else return message.reply("Sorry, I could not find that user.").then(m => del(m, 7500));
+        }
     }
 }
