d78c109027f0500a006a1ea31a1e0ebb8aa4a034
ComicallyBad <amp5646@yahoo.com>
2020-11-15 23:27:07 -0500

    Remove entire guild from database if bot leaves

--- events/guild/guildDelete.js
+++ events/guild/guildDelete.js
@@ -1,7 +1,10 @@
 const db = require("../../schemas/db.js");
+const xp = require("../../schemas/xp.js")
 
 module.exports = (client, guild) => {
     activities = [`${client.guilds.cache.size} servers!`, `${client.channels.cache.size} channels!`, `${client.users.cache.size} users!`], i = 0;
     db.deleteOne({ guildID: guild.id }, {
+    }).catch(err => console.log(err))
+    xp.deleteMany({ guildID: guild.id }, {
     }).catch(err => console.log(err))
 }
