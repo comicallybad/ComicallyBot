a5b3af70ed275db127879ad4a4e46042016ee326
amp5646@yahoo.com <amp5646@yahoo.com>
2020-11-22 14:13:09 -0500

    Good morning command added

--- commands/fun/goodmorning.js
+++ commands/fun/goodmorning.js
@@ -8,20 +8,37 @@ module.exports = {
     description: "Sends a cute goodmorning message.",
     permissions: "member",
     usage: "[Give a goodmorning message]",
-    run: (client, message, args) => {
+    run: async (client, message, args) => {
         const embed = new MessageEmbed()
-        if (args[1]) {
-            embed.addField('Goodmorning Message:', `${args.join(' ')}`);
-        } else embed.addField('Goodmorning Message:', `Goodmorning ${member.displayName} rise and shine!`)
-
-        const member = getMember(message, args.join(" "));
-
-        embed
-            .setFooter(member.displayName, member.user.displayAvatarURL())
-            .setThumbnail(member.user.displayAvatarURL())
-            .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+            .setFooter(`Message from: ${message.member.displayName}`, message.member.user.displayAvatarURL())
             .setTimestamp();
 
-        message.channel.send(embed);
+        if (!args[0]) {
+            embed
+                .setThumbnail(message.member.user.displayAvatarURL())
+                .setColor(message.member.displayHexColor === '#000000' ? '#ffffff' : message.member.displayHexColor)
+                .addField('Goodmorning Message:', `Goodmorning ${message.member.displayName} rise and shine!`)
+
+            return message.channel.send(embed);
+        }
+        if (args[0]) {
+            let member = await message.mentions.users.first() ? message.guild.members.cache.get(message.mentions.users.first().id) : message.member;
+
+            if (member.id !== message.member.id && args[1]) {
+                embed
+                    .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .addField('Goodmorning Message:', `${args.slice(1, args.length).join(' ')}`);
+
+                return message.channel.send(embed);
+            } else if (member.id == message.member.id && args[0]) {
+                embed
+                    .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .addField('Goodmorning Message:', `${args.join(' ')}`);
+
+                return message.channel.send(embed);
+            }
+        }
     }
 }
