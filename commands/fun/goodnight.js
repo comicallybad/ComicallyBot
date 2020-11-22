452283d16314450848f5f26e49bb9683bffca254
amp5646@yahoo.com <amp5646@yahoo.com>
2020-11-22 14:11:38 -0500

    goodnight now can include a personal message

--- commands/fun/goodnight.js
+++ commands/fun/goodnight.js
@@ -7,16 +7,38 @@ module.exports = {
     category: "fun",
     description: "Sends a cute goodnight message.",
     permissions: "member",
-    run: (client, message, args) => {
-        const member = getMember(message, args.join(" "));
-
+    usage: "[@user][Give a goodnight message]",
+    run: async (client, message, args) => {
         const embed = new MessageEmbed()
-            .setFooter(member.displayName, member.user.displayAvatarURL())
-            .setThumbnail(member.user.displayAvatarURL())
-            .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
-            .addField('Goodnight Message:', `Goodnight ${member.displayName} sleep tight`)
+            .setFooter(`Message from: ${message.member.displayName}`, message.member.user.displayAvatarURL())
             .setTimestamp();
 
-        message.channel.send(embed);
+        if (!args[0]) {
+            embed
+                .setThumbnail(message.member.user.displayAvatarURL())
+                .setColor(message.member.displayHexColor === '#000000' ? '#ffffff' : message.member.displayHexColor)
+                .addField('Goodnight Message:', `Goodnight ${message.member.displayName} sleep tight!`)
+
+            return message.channel.send(embed);
+        }
+        if (args[0]) {
+            let member = await message.mentions.users.first() ? message.guild.members.cache.get(message.mentions.users.first().id) : message.member;
+
+            if (member.id !== message.member.id && args[1]) {
+                embed
+                    .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .addField('Goodnight Message:', `${args.slice(1, args.length).join(' ')}`);
+
+                return message.channel.send(embed);
+            } else if (member.id == message.member.id && args[0]) {
+                embed
+                    .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .addField('Goodnight Message:', `${args.join(' ')}`);
+
+                return message.channel.send(embed);
+            }
+        }
     }
 }
