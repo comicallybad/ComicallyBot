9aa90faf384dabba0280e04d9ba9effac54d2d2c
amp5646@yahoo.com <amp5646@yahoo.com>
2021-04-22 02:53:10 -0400

    Avatar command

---
+++ commands/helpful/avatar.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "avatar",
+    aliases: ["av", "profilepic"],
+    category: "helpful",
+    description: "Responds with an embed of a users avatar.",
+    permissions: "member",
+    usage: "[@user | userID]",
+    run: async (client, message, args) => {
+        if (!args[0]) {
+            const embed = new MessageEmbed()
+                .setAuthor(`${message.member.user.tag}`, `${message.author.displayAvatarURL()}`)
+                .setColor("#000000")
+                .setTitle(`**Avatar**`)
+                .setImage(`${message.author.displayAvatarURL({ size: 4096, dynamic: true })}`);
+            return message.channel.send(embed);
+        } else {
+            let member = message.mentions.members.first() || await client.users.fetch(args[0]);
+            if (!member) return message.reply("User not found.").then(m => del(m, 7500));
+            else {
+                console.log(member);
+                const embed = new MessageEmbed()
+                    .setAuthor(`${member.tag}`, `${member.displayAvatarURL()}`)
+                    .setColor("#000000")
+                    .setTitle(`**Avatar**`)
+                    .setImage(`${member.displayAvatarURL({ size: 4096, dynamic: true })}`);
+                return message.channel.send(embed);
+            }
+        }
+    }
+}
