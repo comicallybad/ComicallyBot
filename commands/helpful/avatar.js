bdabbfa43f219067b58a89deb9995aa094f8a425
amp5646@yahoo.com <amp5646@yahoo.com>
2021-04-22 03:42:58 -0400

    Fixed Avatar errors

--- commands/helpful/avatar.js
+++ commands/helpful/avatar.js
@@ -1,3 +1,4 @@
+const { del } = require("../../functions.js")
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
@@ -16,28 +17,15 @@ module.exports = {
                 .setImage(`${message.author.displayAvatarURL({ size: 4096, dynamic: true })}`);
             return message.channel.send(embed);
         } else {
-            let member = message.mentions.members.first();
-            if (member) {
+            let member = message.mentions.members.first() || await message.guild.members.fetch(args[0]).catch(() => { return undefined });
+            if (!member) return message.reply("User not found.").then(m => del(m, 7500));
+            else {
                 const embed = new MessageEmbed()
-                    .setAuthor(`${member.tag}`, `${member.user.displayAvatarURL()}`)
+                    .setAuthor(`${member.user.tag}}`, `${member.user.displayAvatarURL()}`)
                     .setColor("#000000")
                     .setTitle(`**Avatar**`)
                     .setImage(`${member.user.displayAvatarURL({ size: 4096, dynamic: true })}`);
                 return message.channel.send(embed);
-            } else {
-                let member = await client.users.fetch(args[0]);
-                if (!member) return message.reply("User not found.").then(m => del(m, 7500));
-                else {
-                    let member = message.mentions.members.first() || await client.users.fetch(args[0]);
-                    if (member) {
-                        const embed = new MessageEmbed()
-                            .setAuthor(`${member.tag}`, `${member.displayAvatarURL()}`)
-                            .setColor("#000000")
-                            .setTitle(`**Avatar**`)
-                            .setImage(`${member.displayAvatarURL({ size: 4096, dynamic: true })}`);
-                        return message.channel.send(embed);
-                    }
-                }
             }
         }
     }
