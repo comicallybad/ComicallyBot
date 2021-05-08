7d20fa385c02f3618b566d4acd2e2a37fecb206e
amp5646@yahoo.com <amp5646@yahoo.com>
2021-05-07 23:23:34 -0400

    Quick reminder updates

--- commands/helpful/reminder.js
+++ commands/helpful/reminder.js
@@ -13,7 +13,7 @@ module.exports = {
         if (!args[0])
             return message.reply("Please provide a time!");
         if (args[0].match(/^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/) == null)
-            return message.reply("Please provide a valid time: Ex: hh:mm:ss");
+            return message.reply("Please provide a valid time: Ex: hh:mm:ss").then(m => del(m, 7500));
         else if (args[0] && !args[1]) {
             const timeSplit = args[0].split(":");
             const time = ((timeSplit[0] * 3600000) + (timeSplit[1] * 60000) + (timeSplit[2]) * 1000);
@@ -24,7 +24,7 @@ module.exports = {
                 .setDescription(`${message.author}, your timer is finished!`)
                 .setFooter(`Time elapsed: ${humanizeDuration(time)}`, message.author.displayAvatarURL());
 
-            message.reply("Timer set").then(m => del(m, 7500));
+            message.reply(`${humanizeDuration(time)} timer set`).then(m => del(m, 7500));
 
             return setTimeout(() => {
                 message.channel.send(embed);
@@ -36,7 +36,7 @@ module.exports = {
             const reminder = args.join(' ');
 
             if (reminder.length >= 1024)
-                return message.reply("Your reminder must be 1024 characters or less.");
+                return message.reply("Your reminder must be 1024 characters or less.").then(m => del(m, 7500));
 
             const embed = new MessageEmbed()
                 .setColor("#0efefe")
@@ -44,7 +44,7 @@ module.exports = {
                 .setDescription(`${message.author}, I am reminding you: ${reminder}!`)
                 .setFooter(`Time elapsed: ${humanizeDuration(time)}`, message.author.displayAvatarURL());
 
-            message.reply("Reminder set").then(m => del(m, 7500));
+            message.reply(`You will be reminded in ${humanizeDuration(time)}.`).then(m => del(m, 7500));
 
             return setTimeout(() => {
                 message.channel.send(embed);
