3251dc478e28cc2e5d2cecdc4b5b5162c88a96f8
amp5646@yahoo.com <amp5646@yahoo.com>
2021-09-22 07:20:58 -0400

    No more duplicate logs

--- events/antiSpam/muteAdd.js
+++ events/antiSpam/muteAdd.js
@@ -19,19 +19,20 @@ module.exports = (client, member) => {
     }
     let muterole = member.guild.roles.cache.find(r => r.name === "Muted")
     setTimeout(() => {
-        if (!member.roles.cache.some(role => role.name === "Muted")) {
-            member.roles.remove(muterole.id);
-            const embed2 = new MessageEmbed()
-                .setColor("#00ff00")
-                .setTitle("User Unmuted")
-                .setThumbnail(member.guild.me.user.displayAvatarURL())
-                .setFooter(member.guild.me.displayName, member.guild.me.user.displayAvatarURL())
-                .setTimestamp()
-                .setDescription(stripIndents`
-                **Unmuted member:** ${member} (${member.id})
-                **Unmuted by:** ${member.guild.me}
-                **Reason:** 5 Minute mute expired`)
-            if (logChannel) logChannel.send(embed2);
+        if (member.roles.cache.some(role => role.name === 'Muted')) {
+            member.roles.remove(muterole.id).then(() => {
+                const embed2 = new MessageEmbed()
+                    .setColor("#00ff00")
+                    .setTitle("User Unmuted")
+                    .setThumbnail(member.guild.me.user.displayAvatarURL())
+                    .setFooter(member.guild.me.displayName, member.guild.me.user.displayAvatarURL())
+                    .setTimestamp()
+                    .setDescription(stripIndents`
+                    **Unmuted member:** ${member} (${member.id})
+                    **Unmuted by:** ${member.guild.me}
+                    **Reason:** 5 Minute mute expired`)
+                if (logChannel) logChannel.send(embed2);
+            })
         }
     }, 300000)
 }
