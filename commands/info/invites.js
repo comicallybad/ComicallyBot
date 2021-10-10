a40bba0569bb88375cbf3a1f06243e690ce3e378
ComicallyBad <amp5646@yahoo.com>
2021-10-09 23:57:17 -0400

    Fix invites command

--- commands/info/invites.js
+++ commands/info/invites.js
@@ -7,10 +7,15 @@ module.exports = {
     description: "Provides server invite links.",
     permissions: "member",
     run: async (client, message, args) => {
-        const amount = await message.guild.fetchInvites().then(invites => invites.size);
+        if (!message.guild.me.hasPermission("MANAGE_GUILD"))
+            return message.channel.send(`There was an error fetching invites. I need MANAGE_SERVER permissions for this command.`).then(m => del(m, 7500));
+
+        const amount = await message.guild.fetchInvites().then(invites => invites.size).catch(err => { return -1 });
+
+        if (amount == -1) return message.channel.send(`There was an error fetching invites...`).then(m => del(m, 7500));
 
         if (amount > 0) {
-            const invites = await message.guild.fetchInvites().then(invites => invites.map(invite => `**${invite.code}** made by **${invite.inviter.tag}** with **${invite.uses}** uses.`))
+            const invites = await message.guild.fetchInvites().then(invites => invites.map(invite => `**${invite.code}** made by **${invite.inviter.tag}** with **${invite.uses}** uses.`));
             return message.channel.send(invites).then(m => del(m, 30000))
         } else {
             return message.reply("There are no server invites created.").then(m => del(m, 30000))
