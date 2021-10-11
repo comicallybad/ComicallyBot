74d9e19df509d09607c5a7450aa33a989c9c682a
amp5646@yahoo.com <amp5646@yahoo.com>
2021-10-11 02:06:58 -0400

    Auto update bot listing site stats

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,17 +1,18 @@
-const { del } = require("../../functions.js");
 const mongoose = require("mongoose");
 const { dbSetup } = require("../../dbFunctions.js");
 const { stripIndents } = require("common-tags");
+const { AutoPoster } = require('topgg-autoposter')
 
 module.exports = client => {
     const date = new Date()
     console.log(stripIndents`${client.user.username} online. 
-    ${(date.getMonth() + 1).toString().padStart(2, '0')}/${
-        date.getDate().toString().padStart(2, '0')}/${
-        date.getFullYear().toString().padStart(4, '0')} ${
-        date.getHours().toString().padStart(2, '0')}:${
-        date.getMinutes().toString().padStart(2, '0')}:${
-        date.getSeconds().toString().padStart(2, '0')}`
+    ${(date.getMonth() + 1
+        ).toString().padStart(2, '0')}/${date.getDate()
+            .toString().padStart(2, '0')}/${date.getFullYear()
+                .toString().padStart(4, '0')} ${date.getHours()
+                    .toString().padStart(2, '0')}:${date.getMinutes()
+                        .toString().padStart(2, '0')}:${date.getSeconds()
+                            .toString().padStart(2, '0')}`
     );
 
     global.activities = [`${client.guilds.cache.size} servers!`, `${client.channels.cache.size} channels!`, `${client.users.cache.size} users!`], i = 0;
@@ -25,4 +26,8 @@ module.exports = client => {
     dbSetup(client);
 
     client.music.init(client.user.id);
+
+    const ap = AutoPoster('Your Top.gg Token', client)
+
+    ap.on('posted', () => { });
 }
