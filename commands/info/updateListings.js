4a42e466b3d4c3112f43eaea6e54b636665b1bb2
ComicallyBad <amp5646@yahoo.com>
2021-10-12 23:21:23 -0400

    Finally fixed updatelistings to show correct stats

--- commands/info/updateListings.js
+++ commands/info/updateListings.js
@@ -10,15 +10,15 @@ module.exports = {
     run: async (client, message, args) => {
         const discordbotlistngBot = await fetch('https://discordbotlist.com/api/v1/bots/492495421822730250/stats', {
             method: 'post',
-            headers: { "Authorization": `${process.env.DISCORDLISTING}`, "content-type": "application/json" },
-            body: JSON.stringify({ users: client.users.cache.size, guilds: client.guilds.cache.size })
-        }).catch(err => message.reply(`There was an error attempting to update discordbotlists stats. ${err}`));
+            headers: { 'Authorization': process.env.DISCORDBOTLISTING, 'content-type': 'application/json' },
+            body: JSON.stringify({ users: client.users.cache.size, guilds: client.guilds.cache.size, shard_id: 0, voice_connections: 0 })
+        }).then(res => message.channel.send(`Response from discordbotlist \`statusText: ${res.statusText}\``).then(m => del(m, 7500))).catch(err => message.reply(`There was an error attempting to update discordbotlists stats. ${err}`));
         const topgg = await fetch('https://top.gg/api/bots/492495421822730250/stats', {
             method: 'post',
-            headers: { "Authorization": `${process.env.TOPGG}`, "content-type": "application/json" },
-            body: JSON.stringify({ server_count: client.guilds.cache.size })
-        }).catch(err => message.reply(`There was an error attempting to update top.gg stats. ${err}`));
+            headers: { 'Authorization': process.env.TOPGG, 'content-type': 'application/json' },
+            body: JSON.stringify({ server_count: client.guilds.cache.size, shard_id: 0, shard_count: client.shard.count })
+        }).then(res => message.channel.send(`Response from top.gg \`statusText: ${res.statusText}\``).then(m => del(m, 7500))).catch(err => message.reply(`There was an error attempting to update top.gg stats. ${err}`));
 
-        return message.reply(`Update POST sent to discord listing sites.`).then(m => del(m, 7500));
+        return;
     }
 }
