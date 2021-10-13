fe7616825edc847cbddc10fdd402d7a4b63e231d
amp5646@yahoo.com <amp5646@yahoo.com>
2021-10-12 23:48:17 -0400

    After shard update git rebase

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-const { Client, Intents, Collection } = require("discord.js");
-const fs = require("fs");
+const { ShardingManager } = require('discord.js');
 const { config } = require("dotenv");
-const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INVITES,
-Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES]
-const client = new Client({ intents: intents }, { partials: ['GUILD_MEMBER', 'REACTION', 'CHANNEL', 'MESSAGE'] });
-const { Manager } = require("erela.js");
-const AntiSpam = require('discord-anti-spam');
-
-client.music = new Manager({
-    nodes: [{ host: "localhost", port: 2333, password: "ErelaServerPassword!" }], send(id, payload) {
-        const guild = client.guilds.cache.get(id);
-        if (guild) guild.shard.send(payload);
-    },
-});
-
-client.antiSpam = new AntiSpam({
-    warnThreshold: 5, muteThreshold: 7, kickThreshold: 99, banThreshold: 99, maxInterval: 2000,
-    warnMessage: '{@user}, Please stop spamming or you will be muted.',
-    kickMessage: '**{user_tag}** has been kicked for spamming.',
-    muteMessage: '**{user_tag}** has been muted for spamming.',
-    maxDuplicatesWarning: 6, maxDuplicatesKick: 99, maxDuplicatesBan: 99, maxDuplicatesMute: 8,
-    ignoredPermissions: ['MANAGE_NICKNAMES'],
-    ignoreBots: true, verbose: false, muteRoleName: "Muted",
-    removeMessages: true, removeBotMessages: true, removeBotMessagesAfter: 15000,
-});
 
 config({ path: __dirname + "/.env" });
-global.prefix = "=";
-global.voiceChannels = [], global.profanityUsers = [];
 
-["aliases", "commands"].forEach(x => client[x] = new Collection());
-["command", "event", "erela", "antiSpam"].forEach(x => require(`./handlers/${x}`)(client));
-// require(`./handlers/error`)(client, process);
-client.categories = new fs.readdirSync("./commands/");
+const manager = new ShardingManager('./client.js', { token: process.env.TOKEN, totalShards: 'auto', shardList: "auto" });
 
-client.login(process.env.TOKEN);
+manager.on('shardCreate', async (shard) => console.log(`Launched shard ${shard.id}`));
+
+manager.spawn(manager.totalShards, 10000);
