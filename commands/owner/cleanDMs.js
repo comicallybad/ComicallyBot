9846655736530163eb3dddef09f7e6cb880060f4
ComicallyBad <amp5646@yahoo.com>
2021-10-18 01:47:54 -0400

    Added a clean DM's command for owners

---
+++ commands/owner/cleanDMs.js
@@ -0,0 +1,18 @@
+const { del } = require('../../functions.js');
+
+module.exports = {
+    name: "cleandms",
+    aliases: ["dmclean", "cleandm"],
+    category: "info",
+    description: "Cleans DM messages from bot to Owner.",
+    permissions: "admin",
+    run: async (client, message, args) => {
+        let dm = await client.channels.cache.get(process.env.DMCHANNELID);
+        if (!dm) return message.reply("Send the bot a DM, then try this command again.").then(m => del(m, 7500));
+        if (dm) message.reply("Now cleaning your DM's.").then(m => del(m, 7500));
+        dm.messages.fetch().catch(err => err)
+            .then(messages => messages.array().forEach(
+                message => message.author.equals(client.user) && message.delete({ timeout: 0 })
+            )).catch(err => err);
+    }
+}
