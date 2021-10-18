c6360a41b47af291d303d582f230ac9f8117ff15
ComicallyBad <amp5646@yahoo.com>
2021-10-18 03:43:57 -0400

    Fixed Error's of unknown message

--- commands/owner/cleanDMs.js
+++ commands/owner/cleanDMs.js
@@ -12,7 +12,7 @@ module.exports = {
         if (dm) message.reply("Now cleaning your DM's.").then(m => del(m, 7500));
         dm.messages.fetch().catch(err => err)
             .then(messages => messages.array().forEach(
-                message => message.author.equals(client.user) && message.delete({ timeout: 0 })
+                message => message.author.equals(client.user) && message.delete({ timeout: 0 }).catch(err => err)
             )).catch(err => err);
     }
 }
