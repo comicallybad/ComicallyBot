c1ea408de946ea5e376911ffad08769b4c42b602
amp5646@yahoo.com <amp5646@yahoo.com>
2021-10-20 06:08:41 -0400

    New support server link

--- commands/support/botSupportServer.js
+++ commands/support/botSupportServer.js
@@ -7,7 +7,7 @@ module.exports = {
     description: "Provides the support server for the bot.",
     permissions: "member",
     run: (client, message, args) => {
-        const supportServer = "https://discord.gg/jK2JQVc";
+        const supportServer = "Discord: https://discord.gg/mC4J2eETNv";
         return message.reply(`The support server for the discord bot is: ${supportServer}`).then(m => del(m, 15000));
     }
 }
