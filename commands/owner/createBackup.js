9c6f0338078d5f18939a5496b425c13015065954
ComicallyBad <amp5646@yahoo.com>
2021-10-26 06:32:19 -0400

    reverted

--- commands/owner/createBackup.js
+++ commands/owner/createBackup.js
@@ -1,4 +1,5 @@
 const { del } = require("../../functions.js");
+const { stripIndents } = require("common-tags");
 const fs = require("fs");
 const db = require("../../schemas/db.js");
 const xp = require("../../schemas/xp.js");
@@ -9,31 +10,51 @@ module.exports = {
     category: "owner",
     description: "Creates a backup JSON file for the xps and dbs databases.",
     permissions: "owner",
-    run: (client, message, args) => {
+    run: async (client, message, args) => {
+        try{
         if (message.author.id != process.env.USERID)
             return message.reply("You're not the bot the owner!").then(m => del(m, 7500));
 
-        const time = new Date();
-        time.toLocaleString('en-US', { month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+        console.log("inside backups")
 
-        var dir = './backups';
+        const date = new Date();
+        const formatDate = stripIndents`${(date.getMonth() + 1)
+            .toString().padStart(2, '0')}-${date.getDate()
+                .toString().padStart(2, '0')}-${date.getFullYear()
+                    .toString().padStart(4, '0')}`
+        
+        const formatTime = stripIndents`${date.getHours()
+            .toString().padStart(2, '0')}-${date.getMinutes()
+                .toString().padStart(2, '0')}-${date.getSeconds()
+                    .toString().padStart(2, '0')}`
+
+        const dir = './backups';
 
         if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
-        db.find().then(data => {
-            fs.appendFile(`${dir}/${time} dbs-backup.json`, `${data}`, (err) => {
-                if (err) throw err;
-                console.log(`A new warning has been logged to: ${time} warning.log`);
-            });
-            return message.reply("Your dbs database has been backed up.").then(m => del(m, 7500));
-        }).catch(err => message.reply(`There was an error backing up the dbs database. ${err}`).then(m => del(m, 7500)));
+        let guildsID = await client.guilds.cache.map(guild => guild.id);
 
-        xp.find().then(data => {
-            fs.appendFile(`${dir}/${time} xps-backup.json`, `${data}`, (err) => {
-                if (err) throw err;
-                console.log(`A new warning has been logged to: ${time} warning.log`);
+        guildsID.forEach((element, guildIndex) => {
+            db.findOne({ guildID: guildsID[guildIndex] }, (err, exists) => {
+                if(exists) exists.map(guild => guild).forEach(guild => {
+                    fs.appendFile(`./backups/${formatDate} ${formatTime} dbs-backup.json`, `${guild}`, function (err) {
+                    if (err) throw err;
+                        console.log(`A new warning has been logged to: ${formatDate} ${formatTime} warning.log`);
+                    });
+                });
+                xp.findOne({ guildID: guildsID[guildIndex] }, (err, exists) => {
+                    if(exists) exists.map(guild => guild).forEach(guild => {
+                        fs.appendFile(`./backups/${formatDate} ${formatTime} xps-backup.json`, `${guild}`, function (err) {
+                        if (err) throw err;
+                            console.log(`A new warning has been logged to: ${formatDate} ${formatTime} warning.log`);
+                        });
+                    });
+                });
             });
-            return message.reply("Your xps database has been backed up.").then(m => del(m, 7500));
-        }).catch(err => message.reply(`There was an error backing up the xps database. ${err}`).then(m => del(m, 7500)));
+        });
+        return message.reply("Your dbs database and xps database have been backed up.").then(m => del(m, 7500));
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
--- events/error/warning.js
+++ events/error/warning.js
@@ -1,18 +1,30 @@
 const fs = require("fs");
+const { stripIndents } = require("common-tags");
 
 module.exports = async (client, process, error) => {
-    const time = new Date();
-    time.toLocaleString('en-US', { month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+    const date = new Date();
+    const formatDate = stripIndents`
+    ${(date.getMonth() + 1)
+            .toString().padStart(2, '0')}-${date.getDate()
+                .toString().padStart(2, '0')}-${date.getFullYear()
+                    .toString().padStart(4, '0')}`
+
+    const formatTime = stripIndents`${date.getHours()
+        .toString().padStart(2, '0')}-${date.getMinutes()
+            .toString().padStart(2, '0')}-${date.getSeconds()
+                .toString().padStart(2, '0')}`
 
     var dir = './logs';
 
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
 
-    fs.appendFile(`${dir}/${time} warning.log`, `${time}: ${error.stack}\n`, (err) => {
+    fs.appendFile(`./logs/${formatDate} warning.log`, `${formatDate} ${formatTime}: ${error.stack}\n`, function (err) {
         if (err) throw err;
-        console.log(`A new warning has been logged to: ${time} warning.log`);
+        console.log(`A new warning has been logged to: ${formatDate} warning.log`)
     });
 
     let owner = await client.users.cache.get(`${process.env.USERID}`);
-    owner.send(`${time} A new warning: ${error.stack}`).catch(err => console.log(`Could not send warning error message to owner. ${err}`));
+    owner.send(`New warning ${error.stack}`).catch(err => console.log(`Could not send warning error message to owner. ${err}`));
 }
