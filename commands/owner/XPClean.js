90291f8c571d37a86cdabb38441a1c0afd82b456
ComicallyBad <amp5646@yahoo.com>
2021-11-09 04:43:13 -0500

    Accepting changes

--- commands/owner/XPClean.js
+++ commands/owner/XPClean.js
@@ -12,7 +12,24 @@ module.exports = {
             return message.reply("You're not the bot the owner!").then(m => del(m, 7500));
 
         xp.deleteMany({ level: 0, xp: 0 })
-            .catch(err => message.reply(`There was an error deleting unused XP documents ${err}.`).then(m => del(m, 7500)))
-            .then(() => message.reply("XP database has been cleaned up.").then(m => del(m, 7500)))
+            .catch(err => message.reply(`There was an error deleting unused XP documents ${err}.`).then(m => del(m, 7500)));
+
+        const clean = async () => {
+            let guilds = await client.guilds.cache.map(guild => guild);
+            let xpUsers = await xp.find().then(users => users.map(user => user.guildID && user.userID));
+            guilds.forEach(guild => {
+                guild.members.cache.forEach(user => {
+                    if (xpUsers.includes(user.id)) return;
+                    else {
+                        xp.deleteOne({ guildID: guild.id, userID: user.id })
+                            .catch(err => message.reply(`There was an error cleaning empty database listings. ${err}`).then(m => del(m, 7500)));
+                        console.log(`deleted ${guild.id} ${user.id}`);
+                    }
+
+                });
+            });
+            return message.reply("XP database has been cleaned up.").then(m => del(m, 7500));
+        }
+        clean();
     }
 }
