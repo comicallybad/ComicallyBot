3a2c6d42dcb4738f57fcecb3a2a817ca2aa04402
ComicallyBad <amp5646@yahoo.com>
2021-11-13 04:58:26 -0500

    Ready.js Update

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,14 +3,15 @@ const { dbSetup } = require("../../dbFunctions.js");
 
 module.exports = client => {
     var time = new Date();
-    console.log(time.toLocaleString('en-US', { month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }));  
+    console.log(time.toLocaleString('en-US', { month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }));
 
     global.activities = [`${client.guilds.cache.size} servers!`, `${client.channels.cache.size} channels!`, `${client.users.cache.size} users!`], i = 0;
     setInterval(() => client.user.setActivity(`${prefix}help | ${activities[i++ % activities.length]}`, { type: "PLAYING" }), 7500)
 
-    mongoose.connect("mongodb://localhost/ComicallyBOT2", { useUnifiedTopology: true, useNewUrlParser: true }).then(console.log("Successfully connected to Mongodb"));
-
-    dbSetup(client);
+    mongoose.connect("mongodb://localhost/ComicallyBOT2", { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
+        console.log("Successfully connected to Mongodb");
+        dbSetup(client);
+    });
 
     client.music.init(client.user.id);
 }
