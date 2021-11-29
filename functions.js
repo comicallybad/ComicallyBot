478225916eccbf5d3f161ab30207ecadbf34769c
ComicallyBad <amp5646@yahoo.com>
2021-11-28 23:49:38 -0500

    getMember removed

--- functions.js
+++ functions.js
@@ -79,27 +79,6 @@ module.exports = {
         }
     },
 
-    getMember: function (message, toFind = '') {
-        toFind = toFind.toLowerCase();
-
-        let target = message.guild.members.cache.get(toFind);
-
-        if (!target && message.mentions.members)
-            target = message.mentions.members.first();
-
-        if (!target && toFind) {
-            target = message.guild.members.cache.find(member => {
-                return member.displayName.toLowerCase().includes(toFind) ||
-                    member.user.tag.toLowerCase().includes(toFind)
-            });
-        }
-
-        if (!target)
-            target = message.member;
-
-        return target;
-    },
-
     formatDate: function (date) {
         let year = date.getFullYear();
         let month = (1 + date.getMonth()).toString().padStart(2, '0');
