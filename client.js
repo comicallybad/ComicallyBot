567fa9ca980157c28336c64b9230b3a7310563ba
ComicallyBad <amp5646@yahoo.com>
2021-11-29 00:44:36 -0500

    Cleaned up client

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const { config } = require("dotenv");
 const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INVITES,
 Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES];
-const client = new Client({ intents: intents }, { partials: ['GUILD_MEMBER', 'REACTION', 'CHANNEL', 'MESSAGE'] });
+const client = new Client({ intents: intents, partials: ['GUILD_MEMBER', 'REACTION', 'CHANNEL', 'MESSAGE'] });
 const { Manager } = require("erela.js");
 const AntiSpam = require('discord-anti-spam');
 
