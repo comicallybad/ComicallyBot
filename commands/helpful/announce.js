d615209fdd1ccb1f2fd84cb38b73cc788669053e
ComicallyBad <amp5646@yahoo.com>
2021-12-06 21:22:33 -0500

    Fix announce bug

--- commands/helpful/announce.js
+++ commands/helpful/announce.js
@@ -12,13 +12,15 @@ module.exports = {
             return message.reply("Please provide a channel and something to say, or just something to say.").then(m => del(m, 7500));
         } else {
             let channelMentionID = args[0].replace("<#", "").slice(args[0].replace("<#", "").indexOf(":") + 1, args[0].replace("<#", "").length - 1);
-            if (message.mentions.channels.first().id === channelMentionID) {
-                let toSay = args.slice(1).join(' ');
-                let channel = await message.guild.channels.cache.get(message.mentions.channels.first().id);
+            if (message.mentions.channels) {
+                if (message.mentions.channels.first().id === channelMentionID) {
+                    let toSay = args.slice(1).join(' ');
+                    let channel = await message.guild.channels.cache.get(message.mentions.channels.first().id);
 
-                return channel.send(toSay).catch(err => message.reply(`There was an error sending a message to that channel. ${err}`).then(m => del(m, 7500)));
-            } else {
-                return message.channel.send(args.join(' '));
+                    return channel.send(toSay).catch(err => message.reply(`There was an error sending a message to that channel. ${err}`).then(m => del(m, 7500)));
+                } else {
+                    return message.channel.send(args.join(' '));
+                }
             }
         }
     }
