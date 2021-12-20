63f8eb192f750a36c255713ac1e6f6a2a25ca172
ComicallyBad <amp5646@yahoo.com>
2021-12-19 23:03:53 -0500

    Simplified messageDelete

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,22 +1,8 @@
 const db = require("../../schemas/db.js");
 
 module.exports = async (client, message) => {
-    const fetchedLogs = await message.guild.fetchAuditLogs({
-        limit: 1,
-        type: 'MESSAGE_DELETE',
-    });
-
-    const deletionLog = fetchedLogs.entries.first();
-
-    if (!deletionLog) return;
-
-    const { executor, target } = deletionLog;
-    let guildID, messageID;
-
-    if (target.id === message.author.id) {
-        guildID = message.guild.id;
-        messageID = message.id;
-    }
+    const guildID = message.guild.id;
+    const messageID = message.id;
 
     db.updateOne({ guildID: guildID }, {
         $pull: { reactionRoles: { messageID: messageID } }
