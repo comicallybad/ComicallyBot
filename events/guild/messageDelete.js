b0db2fb1354dffff4dcdaa3a288ca3feaf997987
ComicallyBad <amp5646@yahoo.com>
2021-12-20 20:46:47 -0500

    Fixed message delete error

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,6 +1,7 @@
 const db = require("../../schemas/db.js");
 
 module.exports = async (client, message) => {
+    if (!message.guild) return;
     const guildID = message.guild.id;
     const messageID = message.id;
 
