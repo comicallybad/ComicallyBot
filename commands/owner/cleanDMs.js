261954b0e64e149a0eac063972b2157669d2d0d2
ComicallyBad <amp5646@yahoo.com>
2021-12-20 20:46:30 -0500

    Fixed cleanDMs

--- commands/owner/cleanDMs.js
+++ commands/owner/cleanDMs.js
@@ -1,4 +1,4 @@
-const { del } = require('../../functions.js');
+const { r, del } = require('../../functions.js');
 
 module.exports = {
     name: "cleandms",
@@ -8,11 +8,10 @@ module.exports = {
     permissions: "owner",
     run: async (client, message, args) => {
         let dm = await client.channels.fetch(process.env.DMCHANNELID);
-        if (!dm) return r(message.channel, message.author, "Send the bot a DM, then try this command again.").then(m => del(m, 7500));
         if (dm) r(message.channel, message.author, "Now cleaning your DM's.").then(m => del(m, 7500));
-        dm.messages.fetch().catch(err => err)
-            .then(messages => messages.array().forEach(
+        dm.messages.fetch().catch(err => console.log(err.stack))
+            .then(messages => messages.map(m => m).forEach(
                 message => message.author.equals(client.user) && message.delete({ timeout: 0 }).catch(err => err)
-            )).catch(err => err);
+            )).catch(err => console.log(err.stack));
     }
 }
