9ed6d451b0aedfeea2a1e559aef7a24c8719f09d
ComicallyBad <amp5646@yahoo.com>
2022-01-05 09:43:16 -0500

    Color change for leave log

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -21,14 +21,12 @@ module.exports = async (client, member) => {
 
         if (!kickLog) {
             const embed = new MessageEmbed()
-                .setColor("#FF0000")
+                .setColor("#0EFEFE")
                 .setTitle("Member Left")
                 .setThumbnail(member.user.displayAvatarURL())
                 .setFooter(`${member.displayName}`, `${member.user.displayAvatarURL()}`)
                 .setTimestamp()
-                .setDescription(stripIndents`
-                **User Left By:** Most likely leaving on their own will.
-                **User Left:** ${member} (${member.id}`);
+                .setDescription(`**User Left:** ${member} (${member.id}`);
 
             return s(logChannel, '', embed);
         }
