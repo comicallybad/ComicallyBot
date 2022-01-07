cc3bcd61bc443efc3f8d853c49f4cded772df8cc
ComicallyBad <amp5646@yahoo.com>
2022-01-07 04:33:55 -0500

    UpdateListings now represents actual user count

--- commands/owner/updateListings.js
+++ commands/owner/updateListings.js
@@ -10,7 +10,7 @@ module.exports = {
         const discordbotlistngBot = await fetch('https://discordbotlist.com/api/v1/bots/492495421822730250/stats', {
             method: 'post',
             headers: { 'Authorization': process.env.DISCORDBOTLISTING, 'content-type': 'application/json' },
-            body: JSON.stringify({ users: client.users.cache.size, guilds: client.guilds.cache.size, shard_id: 0, voice_connections: 0 })
+            body: JSON.stringify({ users: client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0), guilds: client.guilds.cache.size, shard_id: 0, voice_connections: 0 })
         }).then(res => s(message.channel, `Response from discordbotlist \`statusText: ${res.statusText}\``).then(m => del(m, 7500))).catch(err => r(message.channel, message.author, ` There was an error attempting to update discordbotlists stats. ${err}`));
         const topgg = await fetch('https://top.gg/api/bots/492495421822730250/stats', {
             method: 'post',
