2f73b54edf1010dd68e45dbc0c454ff98bc955ef
ComicallyBad <amp5646@yahoo.com>
2022-01-14 07:34:38 -0500

    Rollback, try catch didn't work nor did .catch

--- commands/owner/run.js
+++ commands/owner/run.js
@@ -19,18 +19,9 @@ module.exports = {
         try {
             if (args.join(" ").toLowerCase().includes("token") || args.join(" ").toLowerCase().includes("steam") || args.join(" ").toLowerCase().includes("erela"))
                 return r(message.channel, message.author, "You cannot find my token :)").then(m => del(m, 7500));
-            try {
-                const toEval = args.join(" ");
-                const evaluated = eval(toEval);
-            } catch (err) {
-                let embed = new MessageEmbed()
-                    .setColor("#FF0000")
-                    .setTitle("\:x: Error!")
-                    .setDescription(`${err}`)
-                    .setFooter(`${client.user.username}`, `${client.user.displayAvatarURL()}`)
 
-                return s(message.channel, '', embed).then(m => del(m, 15000));
-            }
+            const toEval = args.join(" ");
+            const evaluated = eval(toEval);
 
             let embed = new MessageEmbed()
                 .setColor("#00FF00")
