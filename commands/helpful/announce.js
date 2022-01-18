fe2db45f16accb948cda90a3de7ae47b53b0d43f
ComicallyBad <amp5646@yahoo.com>
2022-01-17 22:50:41 -0500

    updated announce in case of error of "embed must be a string"

--- commands/helpful/announce.js
+++ commands/helpful/announce.js
@@ -17,12 +17,12 @@ module.exports = {
                     let toSay = args.slice(1).join(' ');
                     let channel = await message.guild.channels.cache.get(message.mentions.channels.first().id);
 
-                    return s(channel, toSay).catch(err => r(message.channel, message.author, `There was an error sending a message to that channel. ${err}`)).then(m => del(m, 7500));
+                    return s(channel, `${toSay}`).catch(err => r(message.channel, message.author, `There was an error sending a message to that channel. ${err}`)).then(m => del(m, 7500));
                 } else {
-                    return s(message.channel, args.join(' '));
+                    return s(message.channel, `${args.join(' ')}`);
                 }
             } else {
-                return s(message.channel, args.join(' '));
+                return s(message.channel, `${args.join(' ')}`);
             }
         }
     }
