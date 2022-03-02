f81296d9df5694c1183f4f3082356ddba497925a
ComicallyBad <amp5646@yahoo.com>
2022-03-02 00:31:27 -0500

    little grammar change

--- dbFunctions.js
+++ dbFunctions.js
@@ -178,7 +178,7 @@ module.exports = {
                     embed.setDescription(`${user} ${user.user.tag} joined the **${role.roleName}**(${role.roleID})`);
                     if (logChannel) s(logChannel, '', embed);
                     user.roles.add(role.roleID).then(() => {
-                        return r(message.channel, user, `, you have been given the **${role.roleName}** role for: **Ranking up to level ${level}!**`);
+                        return r(message.channel, user, `You have been given the **${role.roleName}** role for: **Ranking up to level ${level}!**`);
                     }).catch(err => {
                         if (err) return s(message.channel, `There was an error assigning XP level role. ${err}`).then(m => del(m, 7500));
                     });
