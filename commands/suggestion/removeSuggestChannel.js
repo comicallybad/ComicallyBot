22f4a66ab7f184c82a9783108b411601bf43034e
ComicallyBad <amp5646@yahoo.com>
2022-03-17 04:47:57 -0400

    Added setSuggestChannel.js, fixed removeSuggestCh

--- commands/suggestion/removeSuggestChannel.js
+++ commands/suggestion/removeSuggestChannel.js
@@ -8,14 +8,14 @@ module.exports = {
     description: "Removes the suggestion channel.",
     permissions: "moderator",
     run: (client, message, args) => {
-        db.findOne({ guildID: message.guild.id, channels: { $elemMatch: { command: "verify" } } }, (err, exists) => {
+        db.findOne({ guildID: message.guild.id, channels: { $elemMatch: { command: "suggest" } } }, (err, exists) => {
             if (exists) {
-                db.updateOne({ guildID: message.guild.id, 'channels.command': "verify" }, {
-                    $pull: { channels: { command: "verify" } }
+                db.updateOne({ guildID: message.guild.id, 'channels.command': "suggest" }, {
+                    $pull: { channels: { command: "suggest" } }
                 }).catch(err => console.log(err));
-                return r(message.channel, message.author, "Removed verify channel.").then(m => del(m, 7500));
+                return r(message.channel, message.author, "Removed suggest channel.").then(m => del(m, 7500));
             }
-            else return r(message.channel, message.author, "There has been no verify channel set.").then(m => del(m, 7500));
+            else return r(message.channel, message.author, "There has been no suggest channel set.").then(m => del(m, 7500));
         }).clone().catch(err => err);
     }
 }
---
+++ commands/suggestion/setSuggestChannel.js
@@ -0,0 +1,48 @@
+const { r, del } = require("../../functions.js");
+const db = require('../../schemas/db.js');
+
+module.exports = {
+    name: "setrankchannel",
+    aliases: ["setrankch", "setrolechannel"],
+    category: "leveling",
+    description: "Set response channel for leveling.",
+    permissions: "moderator",
+    usage: "<#channel | channelID>",
+    run: (client, message, args) => {
+        if (!args[0])
+            return r(message.channel, message.author, "Please provide a channel.").then(m => del(m, 7500));
+
+        let guildID = message.guild.id;
+        let serverChannels = client.channels.cache.map(channel => channel).filter(channel => channel.type === "text").filter(channel => channel.guild.id === guildID)
+        let channelNames = serverChannels.map(channel => channel.name)
+        let channelIDs = serverChannels.map(channel => channel.id)
+        let hashMention = args[0].slice(2, args[0].length - 1)
+
+        if (!channelIDs.includes(hashMention) && !channelIDs.includes(args[0]))
+            return r(message.channel, message.author, "Channel not found in this server.").then(m => del(m, 7500));
+
+        if (channelIDs.includes(args[0]))
+            dbUpdate(args[0], channelNames[channelIDs.indexOf(args[0])]);
+
+        if (channelIDs.includes(hashMention))
+            dbUpdate(hashMention, channelNames[channelIDs.indexOf(hashMention)]);
+
+        function dbUpdate(channelID, channelName) {
+            db.findOne({ guildID: guildID, channels: { $elemMatch: { command: "suggest" } } }, (err, exists) => {
+                if (!exists) {
+                    //push channel if it doesn't exist
+                    db.updateOne({ guildID: guildID }, {
+                        $push: { channels: { command: "suggest", channelID: channelID, channelName: channelName } }
+                    }).catch(err => console.log(err));
+                    return r(message.channel, message.author, "Suggest channel has been set!").then(m => del(m, 7500));
+                } else {
+                    ///update channel if it does exist
+                    db.updateOne({ guildID: guildID, 'channels.command': "suggest" }, {
+                        $set: { 'channels.$.channelID': channelID, 'channels.$.channelName': channelName }
+                    }).catch(err => console.log(err));
+                    return r(message.channel, message.author, "Updated suggestion channel.").then(m => del(m, 7500));
+                }
+            }).clone().catch(err => console.log(err));
+        }
+    }
+}
