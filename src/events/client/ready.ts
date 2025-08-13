367d4ed7008aa9bfa686f0635c2ccb28ff6875cd
ComicallyBad <amp5646@yahoo.com>
2025-08-13 13:11:10 -0400

    fix: reorder init calls

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -14,13 +14,12 @@ export default {
     execute(client: Client) {
         console.log(`${formatLogTimestamp()} [INFO] ${client.user?.tag} (${client.user?.id}) online on shard ${client.shard?.ids[0]}.`);
 
-        initializeActivities(client);
-
         if (!process.env.CLIENT_ID) {
             throw new ValidationError("CLIENT_ID environment variable is not set.");
         }
 
         client.music.init(process.env.CLIENT_ID);
+        initializeActivities(client);
 
         mongoose.set("strictQuery", true);
         mongoose.connect("mongodb://0.0.0.0/ComicallyBot").then(() => {
